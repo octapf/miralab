392433fe2a89731b47484157bad527e51797c04c
Octavio Frangipani <frangipani.octavio@gmail.com>
2025-12-23 12:41:38 -0300

    Actualizar branding completo a Miralab

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -70,8 +70,8 @@ export default function Navbar({ locale }: NavbarProps) {
     <nav className={`${styles.navbar} ${!isVisible ? styles.hidden : ''}`}>
       <div className={styles.container}>
         <div className={styles.logo}>
-          <span className={styles.logoText}>IT</span>
-          <span className={styles.logoAccent}>Solutions</span>
+          <span className={styles.logoText}>Mira</span>
+          <span className={styles.logoAccent}>Lab</span>
         </div>
 
         <ul className={styles.navLinks}>
